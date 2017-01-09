8daf131d25f2cb03e2cc031d5563c7503699824c
ismaila sylla <syldox@gmail.com>
2017-01-09 14:19:57 +0530

    module4-solution

---
+++ script.js
@@ -0,0 +1,12 @@
+(function () {
+   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+   for (var name in names) {
+      var firstLetter = names[name].charAt(0).toLowerCase();
+       if (firstLetter === 'j') {
+          byeSpeaker.speak(names[name]);
+  } else {
+    helloSpeaker.speak(names[name]);
+  }
+}
+
+})();
